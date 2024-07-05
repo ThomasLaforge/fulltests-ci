5d3f6a6238d760efd067afc349125474846dff10
ThomasLaforge <thomas.laforge.38@gmail.com>
2024-07-05 13:29:19 +0200

    update secrets names

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //@ts-ignore
+  test: {
+    include: ['src/**/*.test.ts'], // Example: Only include .test.ts files
+  },
 })
